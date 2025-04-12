40de65aef02264b148e4d9f685fdd16a15472555
Raquel212 <101139920+Raquel212@users.noreply.github.com>
2025-04-12 16:51:16 -0300

    Altera base url do axios

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:8080/",
+    baseURL: "https://medicareapi-dxf6hsargzd0ceer.canadacentral-01.azurewebsites.net/",
     headers: {'Authorization': `Bearer ${localStorage.getItem('authToken')}`}
 });
 
